bb8887ead6d9b81b9817e2668dbe252badf41991
Burak Yauhen <burakyauhen@gmail.com>
2023-01-08 21:10:39 +0300

    feat: add code to index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 module.exports = function toReadable (number) {
-  
+    const dozens = ['twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+    const elevenToNineteen = ['eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+    const units = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten'];
+    let str = '';
+    if (number > 99) {
+        str = `${units[Math.trunc(number / 100) - 1]} hundred`;
+        number = number % 100;
+    }
+    if (number > 19) {
+        str = `${str} ${dozens[Math.trunc(number / 10) - 2]}`;
+        number = number % 10;
+    }
+    if (number >= 11 && number <= 19) {
+        str = `${str} ${elevenToNineteen[number  -11]}`;
+    }
+    if (number >= 1 && number<= 10) {
+        str = `${str} ${units[number -1]}`;
+    }
+    if (str[0] === ' ') {
+        str = str.slice(1);
+    }
+    if (str === '') {
+        str = 'zero';
+    }
+    return str;
 }
